2cd7106cbe8d81cdb96d42c70ce5546c586d096f
A209PC14 <Hong>
2023-09-20 16:29:28 +0700

    lan3

---
+++ 21_2001200114_NguyenThiThuyHong/Form3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _21_2001200114_NguyenThiThuyHong
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+        }
+    }
+}
--- 21_2001200114_NguyenThiThuyHong/Program.cs
+++ 21_2001200114_NguyenThiThuyHong/Program.cs
@@ -16,7 +16,7 @@ namespace _21_2001200114_NguyenThiThuyHong
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new Form3());
         }
     }
 }
